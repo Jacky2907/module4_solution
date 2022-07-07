c94fd2cb9995da379637f4694e18b9a9e4b08764
Jacky <jackyekoua29@gmail.com>
2022-07-07 10:38:38 +0200

    modified

--- script.js
+++ script.js
@@ -52,7 +52,7 @@
     // to lower case character 'j' afterwards.
     // Look up these methods on Mozilla Developer Network web site if needed.
     // var firstLetter =
-    var firstLetter = names[i].charAt(0).toLowerCase()
+    var firstLetter = names[i].charAt(0).toLowerCase() ;
 
     // STEP 12:
     // Compare the 'firstLetter' retrieved in STEP 11 to lower case
@@ -62,10 +62,10 @@
     /* fill in condition here */
     if (firstLetter === 'j') {
       // byeSpeaker.xxxx
-      byeSpeaker.speak(names[i])
+      byeSpeaker.speak(names[i]) ;
     } else {
       // helloSpeaker.xxxx
-      helloSpeaker.speak(names[i])
+      helloSpeaker.speak(names[i]) ;
     }
   }
 })();
